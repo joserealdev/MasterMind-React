9f71046b7ddc3da0e7ffa9839f11bfc07778e97f
Jose Real <joserealdev@gmail.com>
2020-06-16 17:29:15 +0200

    Updates

---
+++ precache-manifest.70f8d9407ff1109988e40fce3ca37e18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bf77681b1bb6f06b0f3fa58c7286bc",
+    "url": "/MasterMind-React/index.html"
+  },
+  {
+    "revision": "774753dcb3fd3d7dfc81",
+    "url": "/MasterMind-React/static/css/main.397bde13.chunk.css"
+  },
+  {
+    "revision": "aad3a95b1f3387ece025",
+    "url": "/MasterMind-React/static/js/2.fdfb15d7.chunk.js"
+  },
+  {
+    "revision": "774753dcb3fd3d7dfc81",
+    "url": "/MasterMind-React/static/js/main.d047767c.chunk.js"
+  },
+  {
+    "revision": "4bf0b2ba6762aa4e372e",
+    "url": "/MasterMind-React/static/js/runtime~main.31a34154.js"
+  }
+]);
